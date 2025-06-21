b2397435fe31bbeb9f2319ac4f07e01b42044f3f
Marcos Morais <marcosmorais.v10@gmail.com>
2025-06-21 00:32:36 -0300

    core

---
+++ index.js
@@ -0,0 +1,24 @@
+const nome = "Artemis"; // Substitua pelo nome desejado
+const xp = 7200;         // Substitua pelo valor de XP desejado
+
+let nivel = "";
+
+if (xp < 1000) {
+  nivel = "Ferro";
+} else if (xp <= 2000) {
+  nivel = "Bronze";
+} else if (xp <= 5000) {
+  nivel = "Prata";
+} else if (xp <= 7000) {
+  nivel = "Ouro";
+} else if (xp <= 8000) {
+  nivel = "Platina";
+} else if (xp <= 9000) {
+  nivel = "Ascendente";
+} else if (xp <= 10000) {
+  nivel = "Imortal";
+} else {
+  nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
